69c30be65636bb43b544e80e7cfa2f557efe855c
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-08-08 18:44:56 +0900

    詳細画面にて表示された文字が改行されるように調整

--- resources/views/user/daily_report/show.blade.php
+++ resources/views/user/daily_report/show.blade.php
@@ -16,7 +16,7 @@
           </tr>
           <tr>
             <th class="table-column">Content</th>
-            <td class='td-text'>{{ $report->contents }}</td>
+            <td class='td-text'>{!! nl2br(e($report->contents)) !!}</td>
           </tr>
         </tbody>
       </table>

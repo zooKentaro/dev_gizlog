356e370163c4c8aa58fcb00d95484b9a834406b7
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-08-10 16:58:11 +0900

    inputタグをlaravelcollectiveで統一

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,13 +5,12 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => 'daily_report.store']) !!}
-      <input class="form-control" name="user_id" type="hidden">
       <div class="form-group form-size-small @if (!empty($errors->first('reporting_time'))) has-error @endif">
-        <input class="form-control" name="reporting_time" type="date">
-        <span class="help-block">{{ $errors->first('reporting_time') }}</span>
+      {!! Form::input('date', 'reporting_time', null, ['class' => 'form-control']) !!}
+      <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
       <div class="form-group @if (!empty($errors->first('title'))) has-error @endif">
-        <input class="form-control" placeholder="Title" name="title" type="text">
+        {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group @if (!empty($errors->first('contents'))) has-error @endif">
--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -5,13 +5,13 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => ['daily_report.update', $report->id], 'method' => 'PUT']) !!}
-      <input class="form-control" name="user_id" type="hidden">
+      {{-- <input class="form-control" name="user_id" type="hidden"> --}}
       <div class="form-group form-size-small @if (!empty($errors->first('reporting_time'))) has-error @endif">
-        <input class="form-control" name="reporting_time" type="date" value="{!! $report->reporting_time->format('Y-m-d') !!}">
+        {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
       <div class="form-group @if (!empty($errors->first('title'))) has-error @endif">
-        <input class="form-control" placeholder="Title" name="title" type="text" value="{{ $report->title }}">
+        {!! Form::input('text', 'title', $report->title, ['placeholder' => 'Title', 'class' => 'form-control']) !!}
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group @if (!empty($errors->first('contents'))) has-error @endif">

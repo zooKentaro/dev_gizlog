d34369e60dd3950c0e8c63f515ecd2d0005521bf
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-08-19 17:58:00 +0900

    変数名をわかりやすいよう修正

--- app/Http/Controllers/User/ReportController.php
+++ app/Http/Controllers/User/ReportController.php
@@ -31,8 +31,8 @@ class ReportController extends Controller
         if (empty($reporting_time)) {
             $reports = $this->report->getByUserId($userId);
         } else {
-            $dt = Carbon::parse($reporting_time);
-            $reports = $this->report->getSearchReports($dt, $userId);
+            $dateTime = Carbon::parse($reporting_time);
+            $reports = $this->report->getSearchReports($dateTime, $userId);
         }
 
         return view('user.daily_report.index', compact('reports'));
